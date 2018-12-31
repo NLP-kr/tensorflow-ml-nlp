8d1b3684fdf1215d57f8efa5a567a8b5e9631f3c
Ryan S. Shin <sungjin7127@gmail.com>
2018-12-31 10:51:21 +0900

    #update

--- 4.TEXT_CLASSIFICATION/sh/in_download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-rsync -avz companyai8way@10.64.50.108:/home/companyai8way/ryan/classifier/data_out/ ./data_out/
--- 4.TEXT_CLASSIFICATION/sh/in_upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-rsync -avz --exclude=".git" --exclude="data_out/" --delete ./ companyai8way@10.64.50.108:/home/companyai8way/ryan/classifier
